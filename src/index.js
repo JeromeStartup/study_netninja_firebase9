4294afe83fe7018cba2e16114dd5d3cb91873461
unknown <startup.jerome@gmail.com>
2022-07-24 13:17:41 +0530

    Adding and delete data Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import {
-    getFirestore, collection,getDocs, doc
+    getFirestore, collection,getDocs, doc,
+    addDoc,deleteDoc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -33,4 +34,32 @@ getDocs(colRef)
 })
 .catch(err =>{
     console.log(err.message)
+})
+
+// adding documents
+const addBookForm=document.querySelector('.add')
+addBookForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    addDoc(colRef,{
+        title: addBookForm.title.value,
+        author: addBookForm.author.value,
+    })
+    .then(()=>{
+        addBookForm.reset()
+    })
+})
+
+//deleting documents
+
+const deleteBookForm=document.querySelector('.delete')
+deleteBookForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const docRef =doc(db,'books',deleteBookForm.id.value)
+    console.log(docRef)
+    
+    deleteDoc(docRef).then(()=>{
+        deleteBookForm.reset()
+    })
 })
